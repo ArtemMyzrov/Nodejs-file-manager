b996b380a8e85e535f2318eec5a5ed3288d75f2b
artemmyzrov <a.myzrov@yandex.ru>
2023-06-23 18:32:53 +0400

    feat: add general completed

--- src/general/index.js
+++ src/general/index.js
@@ -1,17 +1,46 @@
+import readline from 'readline'
+
 const args = process.argv.slice(2)
 const usernameArg = args.find((arg) => arg.startsWith('--username='))
 const username = usernameArg ? usernameArg.split('=')[1] : 'develop'
 console.log(`Welcome to the File Manager, ${username}!`)
+console.log(`You are currently in ${process.cwd()}`)
 
 process.on('SIGINT', () => {
   console.log(`Thank you for using File Manager, ${username}, goodbye!`)
   process.exit()
 })
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim()
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> ',
+})
+
+rl.prompt()
+
+rl.on('line', (input) => {
+  input = input.trim()
+
   if (input === '.exit') {
     console.log(`Thank you for using File Manager, ${username}, goodbye!`)
-    process.exit()
+    rl.close()
+  } else {
+    console.log(`Entered command: ${input}`)
   }
+  if (input !== '.exit') {
+    console.log('Invalid command. Please enter a valid command.')
+  }
+  if (parentDirectory !== rootDirectory) {
+    process.chdir(parentDirectory)
+    console.log(`You are currently in ${process.cwd()}`)
+  } else {
+    console.log('Cannot navigate above the root directory.')
+  }
+
+  rl.prompt()
+})
+
+rl.on('close', () => {
+  process.exit()
 })
